bbd5fc8105372f0f0e09bf832eff7585fb491d20
Tianyi Zheng <tianyi.zheng.ut@gmail.com>
2022-06-19 02:17:26 +0900

    Chapter 1, Part 1 and the beginning of Part 2.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Shawn!")
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About")
+                    .font(.title2)
+                Text("Turtle Rock, being the most prominent rock formation in this area, attracts many tour buses and tourists. Amateur and professional astronomers bring their telescopes and invite the public to observe deep sky objects such as galaxies and globular clusters.")
+            }
             .padding()
+            
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Tianyi Zheng on 2022/06/18.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 33.78, longitude: -115.83),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
