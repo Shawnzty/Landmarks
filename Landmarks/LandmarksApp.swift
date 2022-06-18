407fbba6f2a2fc7d53fbccd141b383c9b2d45c3e
Tianyi Zheng <tianyi.zheng.ut@gmail.com>
2022-06-18 11:38:59 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tianyi Zheng on 2022/06/18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
